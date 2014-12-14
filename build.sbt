bd9ee4c91d8690a217d2708b653d6f96d1f1cfe1
Bartek Kalinka <bartkali@hotmail.com>
2014-12-14 22:29:49 +0100

    added first unit test, failing for now

--- build.sbt
+++ build.sbt
@@ -4,8 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.4"
 
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2"
-
-libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.6.0"
+libraryDependencies ++= List(
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2",
+  "com.github.nscala-time" %% "nscala-time" % "1.6.0",
+  "org.scalatest" %% "scalatest" % "2.1.6" % "test"
+)
 
 mainClass := Some("MinerApp")
---
+++ src/test/scala/LogParserSuite.scala
@@ -0,0 +1,37 @@
+import org.scalatest._
+import com.github.nscala_time.time.Imports._
+
+class LogParserSuite extends FunSuite {
+
+  val input: String =
+    """27.08.2012
+      |24:15
+      |- worklogminer
+      |    - aaa
+      |    - bbb
+      |        - ccc
+      |- other stuff
+      |    - aaa
+      |        - bbb
+      |
+      |24.08.2012
+      |- some minor activity
+      |    - ccc
+      |- worklogminer
+      |    - aaa
+      |    - bbb
+      |
+    """.stripMargin
+
+  test("Parses days with dates") {
+    println(input)
+    val result = LogParser(input)
+    assert(result.length == 2)
+    val day1: Workday = result(0)
+    assert(day1.date == new DateTime(2012, 8, 27, 0, 0))
+    val day2: Workday = result(1)
+    assert(day2.date == new DateTime(2012, 8, 24, 0, 0))
+  }
+
+  //TODO test projects
+}
