e20ff19944ebcf8494250da6f46a917873312411
Bartek Kalinka <bartkali@hotmail.com>
2014-12-16 22:34:36 +0100

    improved dateline

--- src/main/scala/miner.scala
+++ src/main/scala/miner.scala
@@ -7,9 +7,8 @@ case class Workday(date: DateTime, projects: List[Project])
 
 object LogParser extends RegexParsers {
   override def skipWhitespace = false
-  var lineNo: Int = 0
-  def eol: Parser[String] = { lineNo = lineNo + 1; sys.props("line.separator") }
-  val dateRegex = """([0-9]{1,2})\.([0-9]{1,2})\.(20[0-9]{1,2})""".r
+  def eol: Parser[String] = sys.props("line.separator")
+  val dateRegex = """([0-9]{1,2})\.([0-9]{1,2})\.(20[0-9]{1,2}).*""".r
   def dateLine: Parser[DateTime] = dateRegex <~ eol ^^ {
     case dateRegex(d, m, y) => new DateTime(y.toInt, m.toInt, d.toInt, 0, 0)
   }
@@ -20,14 +19,14 @@ object LogParser extends RegexParsers {
     case ~(name, simpleLines) => Project(name, simpleLines)
   }
   def workday: Parser[Workday] = dateLine ~ rep(simpleLine) ~ rep(project) ^^ {
-    case ~(~(date, _), projects) => println(date); Workday(date, projects)
+    case ~(~(date, _), projects) => Workday(date, projects)
   }
   def workdays: Parser[List[Workday]] = rep(workday)
 
   def apply(input: String): List[Workday] = {
     parseAll(workdays, input) match {
       case Success(result, _) => result
-      case failure : NoSuccess => scala.sys.error("line " + lineNo + " " + failure.msg)
+      case failure : NoSuccess => scala.sys.error("line " + failure.next.pos.line + " " + failure.msg)
     }
   }
 }
