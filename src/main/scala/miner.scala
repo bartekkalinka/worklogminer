3e780108cc5d62dfffddfbcb30955a64b6730b81
Bartek Kalinka <bartkali@hotmail.com>
2014-12-16 22:08:03 +0100

    shot at line number in error messages

--- src/main/scala/miner.scala
+++ src/main/scala/miner.scala
@@ -7,7 +7,8 @@ case class Workday(date: DateTime, projects: List[Project])
 
 object LogParser extends RegexParsers {
   override def skipWhitespace = false
-  def eol: Parser[String] = """\r\n""".r
+  var lineNo: Int = 0
+  def eol: Parser[String] = { lineNo = lineNo + 1; """\r\n""".r }
   val dateRegex = """([0-9]{1,2})\.([0-9]{1,2})\.(20[0-9]{1,2})""".r
   def dateLine: Parser[DateTime] = dateRegex <~ eol ^^ {
     case dateRegex(d, m, y) => new DateTime(y.toInt, m.toInt, d.toInt, 0, 0)
@@ -19,14 +20,14 @@ object LogParser extends RegexParsers {
     case ~(name, simpleLines) => Project(name, simpleLines)
   }
   def workday: Parser[Workday] = dateLine ~ opt(simpleLine) ~ rep(project) ^^ {
-    case ~(~(date, lineOpt), projects) => Workday(date, projects)
+    case ~(~(date, lineOpt), projects) => println(date); Workday(date, projects)
   }
   def workdays: Parser[List[Workday]] = rep(workday)
 
   def apply(input: String): List[Workday] = {
     parseAll(workdays, input) match {
       case Success(result, _) => result
-      case failure : NoSuccess => scala.sys.error(failure.msg)
+      case failure : NoSuccess => scala.sys.error("line " + lineNo + " " + failure.msg)
     }
   }
 }
