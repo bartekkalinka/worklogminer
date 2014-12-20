2640c39e232716ac316ba757b828fef8d1d6bc70
Bartek Kalinka <bartkali@hotmail.com>
2014-12-21 00:52:10 +0100

    toString -> case classes

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,4 +1,12 @@
 import com.github.nscala_time.time.Imports._
 
-case class Project(name: String, log: List[String])
-case class Workday(date: DateTime, projects: List[Project])
+case class Project(name: String, log: List[String]) {
+  override def toString = { name }
+}
+
+case class Workday(date: DateTime, projects: List[Project]) {
+  override def toString = {
+    DateTimeFormat.forPattern("dd.MM.yyyy").print(date) + " " +
+      projects.map({ _.toString }).reduce(_ + ", " + _)
+  }
+}
--- src/main/scala/miner.scala
+++ src/main/scala/miner.scala
@@ -6,9 +6,5 @@ object Miner extends App {
   val input: String = io.Source.fromFile("input.txt").mkString
   val res: List[Workday] = LogParser(input)
   println(res.length)
-  res.foreach {
-    case Workday(date, projects) =>
-      println(DateTimeFormat.forPattern("dd.MM.yyyy").print(date) + " " +
-        projects.map({ case Project(name, _) => name }).reduce(_ + ", " + _) )
-  }
+  res.foreach { workday: Workday => println(workday) }
 }
