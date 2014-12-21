6b94bef420f87b15fbfefae10b97cd69501f5eff
Bartek Kalinka <bartkali@hotmail.com>
2014-12-21 15:31:33 +0100

    function cmdline args

--- src/main/scala/miner.scala
+++ src/main/scala/miner.scala
@@ -3,8 +3,12 @@ import com.github.nscala_time.time.Imports._
 import scala.util.parsing.combinator.RegexParsers
 
 object Miner extends App {
-  val input: String = io.Source.fromFile("input.txt").mkString
-  val res: List[Workday] = LogParser(input)
-  println(res.length)
-  res.foreach { workday: Workday => println(workday) }
+  args(0) match {
+    case "parse" =>
+      val input: String = io.Source.fromFile("input.txt").mkString
+      val res: List[Workday] = LogParser(input)
+      println(res.length)
+      res.foreach { workday: Workday => println(workday) }
+
+  }
 }
