eff33f9e34e2976debbffe0e5c5d3e0896d5da47
bartlomiej.kalinka <bartkali@hotmail.com>
2016-10-28 18:30:40 +0200

    serializing log lines to array

--- src/main/scala/pl/bka/Project.scala
+++ src/main/scala/pl/bka/Project.scala
@@ -2,5 +2,9 @@ package pl.bka
 
 case class Project(name: String, log: List[String]) {
   override def toString = { name }
-  def toJson: String = s"""{ "name" : "$name", "log": "${log.reduce(_ + " [newline] " + _ )}" } """
+
+  def toJson: String = {
+    def jsonLine(line: String) = s"""{ "line": "$line" }"""
+    s"""{ "name" : "$name", "log": [ ${log.map(jsonLine).reduce(_ + ", " + _)} ] } """
+  }
 }
